a0a363c585007ae7202ee41d5ce9a02f910b8d35
Christian Keogh <christianfkeogh@gmail.com>
2025-12-09 19:39:35 +0000

    CVE security threat

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <Link href="/">
-      <nav className="bg-red-600 text-white sticky top-0 z-50 shadow-lg">
-        <div className="text-center py-4">
-          <h4 className="text-4xl font-extrabold tracking-wide relative inline-block px-6 py-3 uppercase transition-all duration-300 hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]">
+      <nav className="bg-red-600 text-white sticky top-0 z-50 shadow-lg h-10 p-1">
+        <div className="text-center">
+          <h4 className="text-xl font-extrabold inline-block uppercase transition-all duration-300 hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.8)]">
             <span className="inline-block mr-2 text-blue-700 transition-transform duration-300 hover:scale-125">
               ★
             </span>
